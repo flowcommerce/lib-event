0de946ecc0a9041fef772f117b674cc96f649d1f
mikeclimbrock <roth@flow.io>
2017-05-11 09:15:22 -0400

    refactor code and tests

--- app/io/flow/event/Util.scala
+++ app/io/flow/event/Util.scala
@@ -2,6 +2,8 @@ package io.flow.event
 
 import play.api.libs.json._
 
+import scala.util.{Failure, Success, Try}
+
 object Util {
 
   def parseString(json: JsValue, field: String): Option[String] = {
@@ -17,16 +19,44 @@ object Util {
     }
   }
 
-  def parseLong(json: JsValue, field: String): Option[Long] = {
-    (json \ field).validate[Long] match {
-      case JsError(_) => None
-      case JsSuccess(value, _) => Some(value)
+  def getJsValue(json: JsValue, field: String): JsValue = {
+    (json \ field).validate[JsValue] match {
+      case JsError(_) => sys.error(s"Json value is missing a field named[$field]: $json")
+      case JsSuccess(value, _) => value
     }
   }
 
+  def validateLong(js: JsValue): Either[String, Long] = {
+    js match {
+      case v: JsArray => {
+        v.value.size match {
+          case 1 => validateLong(v.value.head)
+          case _ => Left(s"$v must be a long and not an array")
+        }
+      }
+      case v: JsBoolean => Left(s"$v must be a long and not a boolean")
+      case JsNull => Left(s"$js must be a long and not null")
+      case v: JsNumber => v.asOpt[Long] match {
+        case None => Left(s"$v must be a valid long")
+        case Some(n) => Right(n)
+      }
+      case v: JsObject => Left(s"$v must be a long and not an object")
+      case v: JsString => {
+        Try {
+          v.value.toLong
+        } match {
+          case Success(v) => Right(v)
+          case Failure(_) => Left(s"$js must be a valid long")
+        }
+      }
+    }
+
+  }
+
   def mustParseLong(json: JsValue, field: String): Long = {
-    parseLong(json, field).getOrElse {
-      sys.error(s"Json value is missing a field named[$field]: $json")
+    validateLong(getJsValue(json, field)) match {
+      case Left(errors) => sys.error { s"Could not parse field named[$field] as a long: $errors" }
+      case Right(l) => l
     }
   }
 
--- test/io/flow/event/UtilSpec.scala
+++ test/io/flow/event/UtilSpec.scala
@@ -1,25 +1,53 @@
 package io.flow.event
 
-import io.flow.event.Util
+
 import org.scalatest.{FunSpec, Matchers}
 import play.api.libs.json.Json
+
+import scala.util.{Failure, Success, Try}
 
 class UtilSpec extends FunSpec with Matchers {
 
   it("must parse long") {
-    Util.mustParseLong(Json.parse("""{"id": 123}"""), "id") should be (123)
+    Util.mustParseLong(Json.obj("id" -> 123), "id") should be (123)
+  }
+
+  it("must parse number string as long") {
+    val numberAsString = "123"
+    Util.mustParseLong(Json.obj("id" -> numberAsString), "id") should be (123)
   }
 
   it("must parse max long") {
-    Util.mustParseLong(Json.parse(s"""{"id": ${Long.MaxValue}}"""), "id") should be (Long.MaxValue)
+    Util.mustParseLong(Json.obj("id" -> Long.MaxValue), "id") should be (Long.MaxValue)
   }
 
-  it("fail to parse value that is not a long") {
-    Util.parseLong(Json.parse("""{"id": "123"}"""), "id") should be (None)
+  it("fail to parse value larger than long") {
+    val largeNumber = "12392398349349083459083459083409582093485902345"
+    Try {
+      Util.mustParseLong(Json.obj("id" -> largeNumber), "id")
+    } match {
+      case Success(_) => fail("Test should have failed")
+      case Failure(_) => assert(true)
+    }
+  }
+
+  it("fail to parse character string as long") {
+    val charStr = "abc"
+    Try {
+      Util.mustParseLong(Json.obj("id" -> charStr), "id")
+    } match {
+      case Success(_) => fail("Test should have failed")
+      case Failure(_) => assert(true)
+    }
   }
 
   it("fail to parse value for field that does not exist") {
-    Util.parseLong(Json.parse("""{"bogus": 123}"""), "id") should be (None)
+    Try {
+      Util.mustParseLong(Json.obj("bogus" -> 123), "id")
+    } match {
+      case Success(_) => fail("Test should have failed")
+      case Failure(_) => assert(true)
+    }
   }
 
 }
