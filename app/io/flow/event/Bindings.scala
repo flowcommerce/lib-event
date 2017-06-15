9eeb350afebd456b59292c8abbc3b826e68924ae
Michael Bryzek <mbryzek@alum.mit.edu>
2017-06-15 11:28:06 -0400

    Fully remove v1 implementation of queue

--- app/io/flow/event/Bindings.scala
+++ app/io/flow/event/Bindings.scala
@@ -7,11 +7,9 @@ class QueueModule extends Module {
   def bindings(env: Environment, conf: Configuration) = {
     env.mode match {
       case Mode.Prod | Mode.Dev => Seq(
-        bind[Queue].to[KinesisQueue],
         bind[v2.Queue].to[v2.DefaultQueue]
       )
       case Mode.Test => Seq(
-        bind[Queue].to[MockQueue],
         bind[v2.Queue].to[v2.MockQueue]
       )
     }
