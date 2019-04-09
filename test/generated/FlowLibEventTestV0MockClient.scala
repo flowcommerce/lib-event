0d7e5c1c5f9966483b1f6fdf1155e09885f048ff
Paolo Lim <paolo@flow.io>
2019-04-09 13:47:34 -0400

    Upgrade libraries: io.flow, com.github.ghik

--- test/generated/FlowLibEventTestV0MockClient.scala
+++ test/generated/FlowLibEventTestV0MockClient.scala
@@ -7,22 +7,22 @@ package io.flow.lib.event.test.v0.mock {
 
   object Factories {
 
-    def randomString(): String = {
-      "Test " + _root_.java.util.UUID.randomUUID.toString.replaceAll("-", " ")
+    def randomString(length: Int = 24): String = {
+      _root_.scala.util.Random.alphanumeric.take(length).mkString
     }
 
     def makeTestObject(): io.flow.lib.event.test.v0.models.TestObject = io.flow.lib.event.test.v0.models.TestObject(
-      id = Factories.randomString()
+      id = Factories.randomString(24)
     )
 
     def makeTestObjectDeleted(): io.flow.lib.event.test.v0.models.TestObjectDeleted = io.flow.lib.event.test.v0.models.TestObjectDeleted(
-      eventId = Factories.randomString(),
+      eventId = Factories.randomString(24),
       timestamp = org.joda.time.DateTime.now,
-      id = Factories.randomString()
+      id = Factories.randomString(24)
     )
 
     def makeTestObjectUpserted(): io.flow.lib.event.test.v0.models.TestObjectUpserted = io.flow.lib.event.test.v0.models.TestObjectUpserted(
-      eventId = Factories.randomString(),
+      eventId = Factories.randomString(24),
       timestamp = org.joda.time.DateTime.now,
       testObject = io.flow.lib.event.test.v0.mock.Factories.makeTestObject()
     )
