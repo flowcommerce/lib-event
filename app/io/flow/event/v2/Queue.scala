5b2e8ee42a2526421c6ba835f9fab1f93ec7b6f0
Jean Logeart <jean.logeart@gmail.com>
2019-07-10 10:03:01 -0400

    Revert "Remove metrics (#478)"
    This reverts commit cbbfa18716ac2d17a39eb958ebc0863b3c812e00.

--- app/io/flow/event/v2/Queue.scala
+++ app/io/flow/event/v2/Queue.scala
@@ -4,6 +4,7 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 import io.flow.event.Record
 import io.flow.log.RollbarLogger
+import io.flow.play.metrics.MetricsSystem
 import io.flow.play.util.Config
 import io.flow.util.StreamNames
 import javax.inject.Inject
@@ -65,6 +66,7 @@ trait Producer[T] {
 class DefaultQueue @Inject() (
   config: Config,
   creds: AWSCreds,
+  metrics: MetricsSystem,
   logger: RollbarLogger,
 ) extends Queue with StreamUsage {
 
@@ -97,6 +99,7 @@ class DefaultQueue @Inject() (
         streamConfig[T],
         creds,
         f,
+        metrics,
         logger,
       )
     )
