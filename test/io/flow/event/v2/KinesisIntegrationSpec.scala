16004c01f95c9baf76fcb76d42c08557b8061b07
Ben Iofel <ben@flow.io>
2019-08-02 11:23:41 -0400

    pull localstack image only if not present

--- test/io/flow/event/v2/KinesisIntegrationSpec.scala
+++ test/io/flow/event/v2/KinesisIntegrationSpec.scala
@@ -5,7 +5,8 @@ import cloud.localstack.ServiceName.{KINESIS, DYNAMO}
 import cloud.localstack.docker.LocalstackDocker
 import cloud.localstack.docker.annotation.LocalstackDockerConfiguration
 import org.scalatest.{BeforeAndAfterAll, Suite}
-
+import sys.process._
+import scala.language.postfixOps
 import scala.collection.JavaConverters._
 
 trait KinesisIntegrationSpec extends BeforeAndAfterAll { this: Suite =>
@@ -14,9 +15,11 @@ trait KinesisIntegrationSpec extends BeforeAndAfterAll { this: Suite =>
   override def beforeAll(): Unit = {
     Localstack.teardownInfrastructure()
 
+    val imagePresent = ("docker images" #| "grep localstack" !) == 0
+
     val conf = LocalstackDockerConfiguration.builder()
       .environmentVariables(Map("SERVICES" -> s"$KINESIS,$DYNAMO").asJava)
-      .pullNewImage(true)
+      .pullNewImage(!imagePresent)
       .build()
 
     println(conf)
