6eb2ef885b325360f91884988b4d3f32fe0d5db6
Ben Iofel <ben@flow.io>
2020-04-21 13:55:51 -0400

    update usage of localstack-utils

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,6 @@
 // Comment to get more information during initialization
 logLevel := Level.Warn
 
-credentials += Credentials(Path.userHome / ".ivy2" / ".artifactory")
-
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.0")
 
--- test/io/flow/event/v2/KinesisIntegrationSpec.scala
+++ test/io/flow/event/v2/KinesisIntegrationSpec.scala
@@ -1,20 +1,18 @@
 package io.flow.event.v2
 
 import cloud.localstack.Localstack
-import cloud.localstack.ServiceName.{KINESIS, DYNAMO}
-import cloud.localstack.docker.LocalstackDocker
+import cloud.localstack.ServiceName.{DYNAMO, KINESIS}
 import cloud.localstack.docker.annotation.LocalstackDockerConfiguration
 import org.scalatest.{BeforeAndAfterAll, Suite}
-import sys.process._
-import scala.language.postfixOps
+
 import scala.jdk.CollectionConverters._
+import scala.language.postfixOps
+import scala.sys.process._
 
 trait KinesisIntegrationSpec extends BeforeAndAfterAll { this: Suite =>
-  val localstackDocker = LocalstackDocker.INSTANCE
+  private val localstack = Localstack.INSTANCE
 
   override def beforeAll(): Unit = {
-    Localstack.teardownInfrastructure()
-
     val imagePresent = ("docker images" #| "grep localstack" !) == 0
 
     val conf = LocalstackDockerConfiguration.builder()
@@ -24,10 +22,10 @@ trait KinesisIntegrationSpec extends BeforeAndAfterAll { this: Suite =>
 
     println(conf)
 
-    localstackDocker.startup(conf)
+    localstack.startup(conf)
   }
 
   override def afterAll(): Unit = {
-    localstackDocker.stop()
+    localstack.stop()
   }
 }
