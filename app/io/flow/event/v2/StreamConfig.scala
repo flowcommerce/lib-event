80da85b2dab735e95d8e604519a98fce64dd7b7e
Ben Iofel <ben@flow.io>
2021-02-18 22:14:58 -0500

    delete unused code

--- app/io/flow/event/v2/StreamConfig.scala
+++ app/io/flow/event/v2/StreamConfig.scala
@@ -92,63 +92,6 @@ case class KinesisStreamConfig(
 
     builder.build
   }
-
-
-  //  def toKclConfig(creds: AWSCredentialsProviderChain): KinesisClientLibConfiguration = {
-  //    val dynamoCapacity = {
-  //      FlowEnvironment.Current match {
-  //        case FlowEnvironment.Production => 10 // 10 is the default value in the AWS SDK
-  //        case FlowEnvironment.Development | FlowEnvironment.Workstation => 1
-  //      }
-  //    }
-  //
-  //    @nowarn("cat=deprecation") // https://github.com/awslabs/amazon-kinesis-client/issues/737
-  //    val kclConf = new KinesisClientLibConfiguration(
-  //      appName,
-  //      streamName,
-  //      creds,
-  //      workerId
-  //    ).withTableName(dynamoTableName)
-  //      .withInitialLeaseTableReadCapacity(dynamoCapacity)
-  //      .withInitialLeaseTableWriteCapacity(dynamoCapacity)
-  //      .withInitialPositionInStream(InitialPositionInStream.TRIM_HORIZON)
-  //      .withCleanupLeasesUponShardCompletion(true)
-  //      .withIdleMillisBetweenCalls(idleMillisBetweenCalls.getOrElse(1500L))
-  //      .withMaxRecords(maxRecords.getOrElse(1000))
-  //      .withMaxLeasesForWorker(maxLeasesForWorker.getOrElse(KinesisClientLibConfiguration.DEFAULT_MAX_LEASES_FOR_WORKER))
-  //      .withMaxLeasesToStealAtOneTime(maxLeasesToStealAtOneTime.getOrElse(KinesisClientLibConfiguration.DEFAULT_MAX_LEASES_TO_STEAL_AT_ONE_TIME))
-  //      .withMetricsLevel(MetricsLevel.NONE)
-  //      .withFailoverTimeMillis(30000) // See https://github.com/awslabs/amazon-kinesis-connectors/issues/10
-  //
-  //    endpoints.kinesis.foreach { ep =>
-  //      kclConf.withKinesisEndpoint(ep)
-  //    }
-  //
-  //    endpoints.dynamodb.foreach { ep =>
-  //      kclConf.withDynamoDBEndpoint(ep)
-  //    }
-  //
-  //    kclConf
-  //  }
-  //
-
-//  lazy val kinesisClient: AmazonKinesis = {
-//    val kclb = AmazonKinesisClientBuilder.standard().
-//      withCredentials(awsCredentialsProvider).
-//      withClientConfiguration(
-//        new ClientConfiguration()
-//          .withMaxErrorRetry(10)
-//          .withMaxConsecutiveRetriesBeforeThrottling(1)
-//          .withThrottledRetries(true)
-//          .withConnectionTTL(600000)
-//      )
-//
-//    endpoints.kinesis.foreach { ep =>
-//      kclb.setEndpointConfiguration(new EndpointConfiguration(ep, endpoints.region))
-//    }
-//
-//    kclb.build
-//  }
 }
 
 case class DynamoStreamConfig(
