e8528a2a3d1dde0043e9374b9cd9f72843e02e33
Mike Chimirev <mchimirev@flow.io>
2021-06-23 17:19:26 -0400

    pin localstack

--- test/io/flow/event/v2/KinesisIntegrationSpec.scala
+++ test/io/flow/event/v2/KinesisIntegrationSpec.scala
@@ -2,13 +2,14 @@ package io.flow.event.v2
 
 import cloud.localstack.Localstack
 import cloud.localstack.ServiceName.{CLOUDWATCH, DYNAMO, DYNAMO_STREAMS, KINESIS}
-import cloud.localstack.docker.annotation.LocalstackDockerConfiguration
+import cloud.localstack.docker.annotation.{LocalstackDockerConfiguration, LocalstackDockerProperties}
 import org.scalatest.{BeforeAndAfterAll, Suite}
 
 import scala.jdk.CollectionConverters._
 import scala.language.postfixOps
 import scala.sys.process._
 
+@LocalstackDockerProperties(imageTag = "84f0b1b7bf69")
 trait KinesisIntegrationSpec extends BeforeAndAfterAll { this: Suite =>
   private val localstack = Localstack.INSTANCE
 
